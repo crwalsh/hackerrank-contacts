ec4d03bd70a4497f3489efb1b3c588cc7d4cca1a
Lynsey Overturf <lro2118@columbia.edu>
2026-07-27 21:49:17 -0400

    added initial logic

--- firstpass.c
+++ firstpass.c
@@ -10,3 +10,18 @@
 
 #define L_SIZE (OPERATION_BUF_SIZE + NAME_BUF_SIZE + 1)
 
+
+typedef enum {
+        add,
+        find,
+        search,
+        incorrect,
+}CommandType;
+
+CommandType getInputValue(char s[]){
+
+        if(strcmp(s, "add") == 0){
+                return add;
+        }
+
+        if(strcmp(s, "find") == 0){
