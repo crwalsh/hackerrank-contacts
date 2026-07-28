0b4846878467c7a26a1762ccade7e12e5ddc743c
Lynsey Overturf <lro2118@columbia.edu>
2026-07-27 21:48:57 -0400

    created file

---
+++ firstpass.c
@@ -0,0 +1,12 @@
+/*Lynsey Ciaran Ashley*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define ALPHABET_LENGTH 26
+#define OPERATION_BUF_SIZE 7
+#define NAME_BUF_SIZE 22
+
+#define L_SIZE (OPERATION_BUF_SIZE + NAME_BUF_SIZE + 1)
+
